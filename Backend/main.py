bfc5ffdb7ac56a09bf68a0e6e647d4ebf0f2908a
Fikresilase <shitubilbila@gmail.com>
2025-08-13 08:28:13 +0300

    post request works completely

--- Backend/main.py
+++ Backend/main.py
@@ -1,14 +1,31 @@
-#imprt statments
+#import modules
+from fastapi import FastAPI
 from google import genai
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
-
-#load function
-load_dotenv()
+#Run modules
+load_dotenv()   
+app = FastAPI()
 client = genai.Client()
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="who are u"
-)
-print(response.text)
+#post route goes in here
+# @app.get("/ai")
+# def get_ai_explanation():
+#     response = client.models.generate_content(
+#         model="gemini-2.5-flash",
+#         contents="Explain how AI works in a few words",
+#     )
+#     return {"text": response.text}
+# Define what the POST request body should look like
+class PromptRequest(BaseModel):
+    prompt: str
+
+@app.post("/")
+def generate_ai_text(request: PromptRequest):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=request.prompt
+    )
+    return {"text": response.text}
+
